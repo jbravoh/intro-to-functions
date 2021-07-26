64d32796b71f43fb1b1229d26fd2e1bd256c8d27
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-07-26 15:34:11 +0100

    Complete challenge 1

---
+++ challenges/1-longest-word-in-list.js
@@ -0,0 +1,13 @@
+function longestInList(words) {
+    let longest = words[0];
+    for (let word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+    return longest;
+}
+
+console.log(longestInList(["England", "Wales", "Scotland", "Northern Ireland"]), 'should be "Northern Ireland"');
+console.log(longestInList(["the", "quick", "brown", "fox"]), 'should be "quick"');
+console.log(longestInList(["hello", "hi", "greetings", "hey"]), 'should be "greetings"')
